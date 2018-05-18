48bdcc318d8745a58b7f0a2f7dae8f2bf5e21ef8
min Z <740132087@qq.com>
2018-05-18 11:08:22 +0800

    插入数据

---
+++ insert.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangmin
+ * Date: 2018/5/11
+ * Time: 15:05
+ */
+?>
+<html>
+<body>
+
+<form action="MySQL.php" method="post"/><br/>
+    cno: <input type="int" name="cno" size="11" value="" /><br/>
+    cname: <input type="char" name="cname" size="20" value=""/><br/>
+    teacher: <input type="varchar" name="teacher" size="10" value=""/><br/>
+    cplace: <input type="varchar" name="cplace" size="20" value=""/><br/>
+    <input type="submit" />
+</form>
+
+</body>
+</html>
